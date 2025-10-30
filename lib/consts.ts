6377abb84e7a68505e0e04b41c91bd1b3e0da2ee
Lokesh Kumar <lokesh.kumar@fullthrottlelabs.com>
2025-10-30 19:54:36 +0530

    Add agent deployment capabilites and ci/cd

--- lib/consts.ts
+++ lib/consts.ts
@@ -40,75 +40,75 @@ export const openPhoneConversation: Array<CustomUIMessage> = [
         ],
     },
     {
-    id: "msg-openphone-4",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "button",
-        text: "Start capture",
-        action: "start_capture"
-      }
-    ],
-  },  {
-    id: "msg-openphone-5",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "text",
-        text: "Click on Send a message"
-      },
-    ],
-  },
-  {
-    id: "msg-openphone-6",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "text",
-        text: "Click on Commands"
-      },
-    ],
-  },
-  {
-    id: "msg-openphone-7",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "text",
-        text: "Click on /snippets"
-      },
-    ],
-  },
-  {
-    id: "msg-openphone-8",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "text",
-        text: "Click on Post Thumbtack Outreach For Meal Prep…"
-      },
-    ],
-  },  {
-    id: "msg-openphone-9",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "reasoning",
-        text: "Analyzing steps\nSteps stored in memory",
-        status: "streaming",
-      }
-    ],
-  },
+        id: "msg-openphone-4",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "button",
+                text: "Start capture",
+                action: "start_capture"
+            }
+        ],
+    }, {
+        id: "msg-openphone-5",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "text",
+                text: "Click on Send a message"
+            },
+        ],
+    },
     {
-    id: "msg-openphone-10",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "text",
-        text: " Great. I have studied how you use an Openphone, I will follow these steps. Would you like me to follow up with the client in 24 hours if I don't hear back?"
-      },
-    ],
-  },
+        id: "msg-openphone-6",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "text",
+                text: "Click on Commands"
+            },
+        ],
+    },
+    {
+        id: "msg-openphone-7",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "text",
+                text: "Click on /snippets"
+            },
+        ],
+    },
+    {
+        id: "msg-openphone-8",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "text",
+                text: "Click on Post Thumbtack Outreach For Meal Prep…"
+            },
+        ],
+    }, {
+        id: "msg-openphone-9",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "reasoning",
+                text: "Analyzing steps\nSteps stored in memory",
+                status: "streaming",
+            }
+        ],
+    },
+    {
+        id: "msg-openphone-10",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "text",
+                text: " Great. I have studied how you use an Openphone, I will follow these steps. Would you like me to follow up with the client in 24 hours if I don't hear back?"
+            },
+        ],
+    },
     {
         id: "msg-openphone-11",
         role: "user",
@@ -146,16 +146,16 @@ export const openPhoneConversation: Array<CustomUIMessage> = [
         ],
     },
     {
-    id: "msg-openphone-14",
-    role: "ai-agent",
-    parts: [
-      {
-        type: "reasoning",
-        text: "Analyzing steps\nSteps stored in memory",
-        status: "streaming",
-      }
-    ],
-  },
+        id: "msg-openphone-14",
+        role: "ai-agent",
+        parts: [
+            {
+                type: "reasoning",
+                text: "Analyzing steps\nSteps stored in memory",
+                status: "streaming",
+            }
+        ],
+    },
     {
         id: "msg-openphone-15",
         role: "ai-agent",
@@ -184,6 +184,11 @@ export const openPhoneConversation: Array<CustomUIMessage> = [
             {
                 type: "text",
                 text: "Ok got it. Stackbirds Sales Agent is ready to go. "
+            },
+            {
+                type: "button",
+                text: "Deploy Now",
+                action: "deploy_agent"
             }
         ],
     }
